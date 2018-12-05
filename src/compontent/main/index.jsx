4f58a7890916e45061568248c48d89771501a1bf
cj <18137774728@163.com>
2018-12-05 16:09:57 +0800

    傻逼git

---
+++ src/compontent/main/index.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+export default class Main extends Component{
+
+  render(){
+
+
+    return(
+        <div>
+          入口文件
+        </div>
+      )
+
+
+
+}
+
+}
